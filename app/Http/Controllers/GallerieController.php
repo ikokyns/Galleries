0a3efa213c3379107142593d65f70441cf4634ad
ikokyns <momcilo87@hotmail.com>
2017-09-11 20:08:44 +0200

    Created migration for DB

---
+++ app/Http/Controllers/GallerieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gallerie;
+use Illuminate\Http\Request;
+
+class GallerieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Gallerie  $gallerie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gallerie $gallerie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Gallerie  $gallerie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gallerie $gallerie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Gallerie  $gallerie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gallerie $gallerie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Gallerie  $gallerie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gallerie $gallerie)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_09_11_172058_create_galleries_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('image_id');
+            
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropForeign('galleries_user_id_foreign');
+            $table->dropForeign('galleries_image_id_foreign');
+        });
+        Schema::dropIfExists('galleries');
+
+    }
+}
